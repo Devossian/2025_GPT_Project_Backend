fe95eae8e534377bd5ef5d36478647b88f9a2e2f
alstj2384 <ass91160@naver.com>
2025-01-20 16:40:50 +0900

    FEAT: CustomUser model 설정

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    balance = models.DecimalField(
+        max_digits=12,  # 최대 자리수 (10억까지 저장 가능)
+        decimal_places=2,  # 소수점 자리수 (예: 소수점 둘째 자리까지)
+        default=0.00  # 초기값
+    )
+
+    def __str__(self):
+        return self.username
