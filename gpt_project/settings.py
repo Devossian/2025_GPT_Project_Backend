18d902f12d851bdff0c5ab76a3718c35a541c975
alstj2384 <ass91160@naver.com>
2025-01-27 12:13:17 +0900

    BUILD: redis 추가

--- gpt_project/settings.py
+++ gpt_project/settings.py
@@ -93,6 +93,17 @@ DATABASES = {
     }
 }
 
+CACHE = {
+    "default" : {
+        "BACKEND" : "django_redis.cache.RedisCache",
+        "LOCATION" : "redis://127.0.0.1:6379/1",
+        "OPTION" : {
+            "CLIENT_CLASS" : "django_redis.client.DefaultClient",
+        }
+    }
+
+}
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
