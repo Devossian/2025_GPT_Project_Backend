f652fef77840f512b43d7ee88b616309f0973047
kimbabZIP <02legend.park@gmail.com>
2025-01-30 13:51:06 +0900

    gpt 기억 기능 추가

--- gpt_api/urls.py
+++ gpt_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PostGPTAPI
 
 urlpatterns = [
-    path('post-gpt-api', PostGPTAPI.as_view(), name='post_gpt_api'),
+    path('chat/send-message', PostGPTAPI.as_view(), name='post_gpt_api'),
 ]
