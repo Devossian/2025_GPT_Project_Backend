445224beeee60a6076ce18f9351172c32fa8c21a
alstj2384 <ass91160@naver.com>
2025-02-03 13:33:33 +0900

    CHORE: redis 설정 변경

--- gpt_project/settings.py
+++ gpt_project/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
 CACHES = {
     "default" : {
         "BACKEND" : "django_redis.cache.RedisCache",
-        "LOCATION" : "redis://127.0.0.1:6379/1",
+        "LOCATION" : "redis:6379",
         "OPTION" : {
             "CLIENT_CLASS" : "django_redis.client.DefaultClient",
         }
