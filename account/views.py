0447e2d2131eb42edc73bfe89e1e386be5d9e86e
KimbabZIP <02legend.park@gmail.com>
2025-03-07 15:31:03 +0900

    오타 수정

--- account/views.py
+++ account/views.py
@@ -210,7 +210,7 @@ def login(request):
 def user_info(request): # 임의로 작성한 User 정보 get 요청 api
     username = request.GET.get("username")
     try :
-        user = CustomUser.bjects.get(username=username)
+        user = CustomUser.objects.get(username=username)
         return Response({"username": user.username, "balance": user.balance}, status=200)
     except Exception as e :
         return Response({"message": str(e)})
